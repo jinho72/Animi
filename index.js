c0085a5002345b92fccc49ec2ba4b088e18ab59b
Jinho Yang <70585522+jinho72@users.noreply.github.com>
2025-11-28 12:27:35 -0500

    Fix string formatting for music track URLs

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ import {
 // ---------- BACKGROUND MUSIC TRACKS ----------
 // resorted to cdn.jsdelivr for fast loading
 const musicTracks = [
-  https://cdn.jsdelivr.net/gh/jinho72/Animi@master/meditation-music-338902.mp3,
-  https://cdn.jsdelivr.net/gh/jinho72/Animi@master/meditation-background-409198.mp3,
-  https://cdn.jsdelivr.net/gh/jinho72/Animi@master/meditation-background-434654.mp3
+  "https://cdn.jsdelivr.net/gh/jinho72/Animi@master/meditation-music-338902.mp3",
+  "https://cdn.jsdelivr.net/gh/jinho72/Animi@master/meditation-background-409198.mp3",
+  "https://cdn.jsdelivr.net/gh/jinho72/Animi@master/meditation-background-434654.mp3"
 ];
 
 // ---------- DOM ELEMENTS ----------
